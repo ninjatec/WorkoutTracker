5b503e82cad12f21a48cf4c65bef882e124a249a
marc.coxall <marc.coxall@ninjatec.co.uk>
2025-04-22 08:44:32 +0100

    Deploy version 1.5.1.9

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('1.5.1.8', 'd4515b9', CURRENT_TIMESTAMP);
+VALUES ('1.5.1.9', 'a65afa9', CURRENT_TIMESTAMP);
