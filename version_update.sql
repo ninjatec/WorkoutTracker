530836c4ff03b6da3551619c048591cc46998b16
Marc Coxall <marc.coxall@smarsh.com>
2025-04-22 10:19:08 +0100

    Deploy version 1.5.1.15

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('1.5.1.9', 'a65afa9', CURRENT_TIMESTAMP);
+VALUES ('1.5.1.15', '5f44e38', CURRENT_TIMESTAMP);
