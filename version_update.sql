af61fbca7115e489e3bcd4b47f29072c52c5e4cf
Marc Coxall <marc.coxall@smarsh.com>
2025-04-24 17:58:45 +0100

    Deploy version 1.7.3.8

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('1.7.3.7', '6c009b1', CURRENT_TIMESTAMP);
+VALUES ('1.7.3.8', '53684ab', CURRENT_TIMESTAMP);
