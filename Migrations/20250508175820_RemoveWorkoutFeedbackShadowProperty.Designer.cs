29f66b2f59318691ab0faae9579132edb67d6fff
marc.coxall <marc.coxall@ninjatec.co.uk>
2025-05-08 19:12:46 +0100

    DB Merge prep

--- Migrations/20250508175820_RemoveWorkoutFeedbackShadowProperty.Designer.cs
+++ Migrations/20250508175820_RemoveWorkoutFeedbackShadowProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkoutTrackerWeb.Data;
 
@@ -11,9 +12,11 @@ using WorkoutTrackerWeb.Data;
 namespace WorkoutTrackerWeb.Migrations
 {
     [DbContext(typeof(WorkoutTrackerWebContext))]
-    partial class WorkoutTrackerWebContextModelSnapshot : ModelSnapshot
+    [Migration("20250508175820_RemoveWorkoutFeedbackShadowProperty")]
+    partial class RemoveWorkoutFeedbackShadowProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
