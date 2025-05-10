afbc30efbddfc20c2bc28f7227b15df574ef3ff5
marc.coxall <marc.coxall@ninjatec.co.uk>
2025-05-10 08:10:59 +0100

    Create new DB objects

---
+++ Data/Migrations/WorkoutTrackerWeb/20250510070914_AddThemePreferenceToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutTrackerWeb.Data.Migrations.WorkoutTrackerWeb
+{
+    /// <inheritdoc />
+    public partial class AddThemePreferenceToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThemePreference",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThemePreference",
+                table: "AspNetUsers");
+        }
+    }
+}
