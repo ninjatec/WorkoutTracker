2e729219634ff5f09785fccfb15c094d7fc5595f
Jenkins CI <marc.coxall@ninjatec.co.uk>
2025-05-14 09:58:58 +0000

    Deploy version 2.3.2.3

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('2.3.2.2', '49a2cdc9a767ac270661d803121009085527e651', CURRENT_TIMESTAMP);
+VALUES ('2.3.2.3', '3f2b4a1d8f71884dbf950f7231d861030facb100', CURRENT_TIMESTAMP);
