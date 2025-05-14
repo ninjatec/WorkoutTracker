a827a5cf502da4f85e542d0f616aa3500dc0eefb
Ninjatec <ninjatec@users.noreply.github.com>
2025-05-14 12:25:11 +0100

    Bring dev up to date (#25)
    * Deploy version 2.2.0.3
    
    * Deploy version 2.2.0.4
    
    * Deploy version 2.2.1.1
    
    * Deploy version 2.2.1.2
    
    * Deploy version 2.2.1.3
    
    * Deploy version 2.2.1.4
    
    * Deploy version 2.2.1.5
    
    * Deploy version 2.2.1.6
    
    * Deploy version 2.2.1.7
    
    * Deploy version 2.2.2.1
    
    * Deploy version 2.2.2.2
    
    * Create dotnet-desktop.yml github action
    
    * Update dotnet-desktop.yml
    
    * Update dotnet-desktop.yml
    
    * remove broken action
    
    * Deploy version 2.2.2.3
    
    * Deploy version 2.2.2.4
    
    * Deploy version 2.2.3.1
    
    * Deploy version 2.2.4.1
    
    * Deploy version 2.2.4.2
    
    * Deploy version 2.2.5.1
    
    * Deploy version 2.2.5.2
    
    * Deploy version 2.2.5.3
    
    * Deploy version 2.2.5.4
    
    * Deploy version 2.3.0.1
    
    * Deploy version 2.3.1.1
    
    * Deploy version 2.3.2.1
    
    * wot jenkins
    
    * wot2
    
    * JenkinsFile Updates
    
    * jenkins
    
    * docker login in jenkinsfile
    
    * jenkinsfile
    
    * jenkinsfile
    
    * jenkins
    
    * jenkins
    
    * repo
    
    * jenkins
    
    * jenkinsfile
    
    * docker auth in jenkinsfile
    
    * Jenkins fac
    
    * jenkins
    
    * jenkinsfile
    
    * json
    
    * jenkinsfile
    
    * fix
    
    * jenkins
    
    * dcde
    
    * sqlcmd
    
    * sqlcmd
    
    * WoT
    
    * dd
    
    * sqlcmd
    
    * sql version update fix
    
    * jenkinsfile
    
    * WoT
    
    * jf
    
    * jk
    
    * jenkins
    
    * WoT
    
    * WoT Jenkins
    
    * WoT
    
    * fixes for git push
    
    * fixes for git push
    
    * Deploy version 2.3.2.2
    
    * JK Final Tweaks
    
    * params
    
    * Fix JK
    
    * Deploy version 2.3.2.3
    
    * Deploy version 2.3.2.4
    
    * Deploy version 2.3.2.5
    
    * Deploy version 2.3.2.6
    
    * Email Reciept
    
    * Deploy version 2.3.2.7
    
    * Add CI Skip
    
    * Deploy version 2.3.2.8 [ci skip]
    
    * Deploy version 2.3.2.9 [ci skip]
    
    * [ci skip] Update jenkinsfile
    
    * [ci skip] Deploy version 2.3.2.10 [ci skip]
    
    * [ci skip] Deploy version 2.3.2.11 [ci skip]
    
    * [ci skip] Add CI Skip to pipeline
    
    * Add Log to email
    
    * [ci skip] Deploy version 2.3.2.12 [ci skip]
    
    * [ci skip] CI Remove Email on Abort
    
    ---------
    
    Co-authored-by: Marc Coxall <marc.coxall@smarsh.com>

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('2.2.0.2', '53f360d', CURRENT_TIMESTAMP);
+VALUES ('2.3.2.12', '6cd6d3bab624124003d64695cc46d95d357c76f5', CURRENT_TIMESTAMP);
