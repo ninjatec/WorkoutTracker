41f63f717e80d490b1cae9122c3c412daff77da8
Ninjatec CI <marc.coxall@ninjatec.co.uk>
2025-05-14 09:37:27 +0000

    Deploy version 2.3.2.2

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('2.3.2.1', '4ae9363', CURRENT_TIMESTAMP);
+VALUES ('2.3.2.2', '49a2cdc9a767ac270661d803121009085527e651', CURRENT_TIMESTAMP);
