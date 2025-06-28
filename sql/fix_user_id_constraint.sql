140ea38e346be397bf49b6e63fbd109d1b3ec2b4
marc.coxall <marc.coxall@ninjatec.co.uk>
2025-06-28 09:22:16 +0100

    Tidy Up Repo

--- sql/fix_user_id_constraint.sql
+++
@@ -1,3 +0,0 @@
-IF OBJECT_ID('FK_WorkoutSessions_User_UserId1', 'F') IS NOT NULL
-    ALTER TABLE WorkoutSessions DROP CONSTRAINT FK_WorkoutSessions_User_UserId1;
-GO
--- sql/version_update.sql
+++
@@ -1,3 +0,0 @@
--- Recording deployment version
-INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('2.0.2.2', '4c8ce72', CURRENT_TIMESTAMP);
