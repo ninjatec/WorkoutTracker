445cfbce1fda9497cf9caa9e6033e8bcb6a12893
Jenkins CI <marc.coxall@ninjatec.co.uk>
2025-07-01 18:52:06 +0000

    [ci skip] Deploy version 2.4.2.18 [ci skip]

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('2.4.2.17', '2ef50c3e49ef568530aa7c59fd94e7b21e9b3e14', CURRENT_TIMESTAMP);
+VALUES ('2.4.2.18', '9074b68270aad5c1db436387310588da15e10bcf', CURRENT_TIMESTAMP);
