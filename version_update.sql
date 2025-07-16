69d26fbe9ab03727cae338f189c1db8831cb2358
Ninjatec <ninjatec@users.noreply.github.com>
2025-07-16 10:00:46 +0100

    [ci skip] Deploy version 2.4.4.4 [ci skip] (#58)

--- version_update.sql
+++ version_update.sql
@@ -1,3 +1,3 @@
 -- Recording deployment version
 INSERT INTO Deployments (Version, GitCommit, DeploymentDate)
-VALUES ('2.4.4.3', '7ad78ad1081bd070c69c8e4dd9c41502e0f18aa2', CURRENT_TIMESTAMP);
+VALUES ('2.4.4.4', '18c6a1e4048197294a87207099b78b5d4e1270ff', CURRENT_TIMESTAMP);
